1d30db80cdf4b45ba2ce96a262ae694498a88f43
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-12-01 10:43:17 +0900

    add: Slide Component

--- rononaboard-web/src/pages/index.js
+++ rononaboard-web/src/pages/index.js
@@ -1,5 +1,16 @@
-import * as React from "react"
+import React from "react"
+import Slide from "../components/slide";
 
-export default function Home() {
-  return <div>Hello world!</div>
+function App() {
+  const thirdSlideTitle = "예방 행동 수칙";
+  return (
+    <div>
+      <h1>코로나보드(예제,버-전!)</h1>
+      <Slide title="국가별 현황">국가별 현황을 보여줍니다.</Slide>
+      <Slide title="대한민국 지역별 현황">대한민국 지역별 현황을 보여줍니다.</Slide>
+      <Slide title={thirdSlideTitle}>예방행동 수칙을 보여줍니다.</Slide>
+    </div>
+  );
 }
+
+export default App;
