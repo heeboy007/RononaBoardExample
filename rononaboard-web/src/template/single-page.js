8d60d4c5f283945822f6b4b0fee0dd240b29bac5
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-12-02 10:37:26 +0900

    add: node api based page

---
+++ rononaboard-web/src/template/single-page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Silde from "../components/slide";
+import Slide from "../components/slide";
+
+function SinglePage({ pageContext }) {
+    const { dataSource } = pageContext;
+    const { thirdSlideTitle } = dataSource;
+
+    return (
+        <div>
+            <h1>코로나 보드</h1>
+            <p>createPages로 만들어진 페이지 입니다.</p>
+            <Slide title="국가별 현황">국가별 현황을 보여줍니다.</Slide>
+            <Slide title="대한민국 지역별 현황">대한민국 지역별 현황을 보여줍니다.</Slide>
+            <Slide title={thirdSlideTitle}>예방행동 수칙을 보여줍니다.</Slide>
+        </div>
+    )
+}
+
+export default SinglePage;
